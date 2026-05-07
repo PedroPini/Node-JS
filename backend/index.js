f38aef1cbfdbc3281a1ac579fc3b3c7865f2873c
PedroPini <augustopini@gmail.com>
2026-05-07 20:28:23 +0800

    Adding all the other files

---
+++ backend/index.js
@@ -0,0 +1,25 @@
+//CommonJS
+// const express = require('express')
+//ESModule
+import express from 'express'
+const app = express()
+const port = 3000
+
+//http://localhost:3000/
+app.get('/', (req, res) => {
+  console.log("Hurray we're at GET method")
+  res.send('Hello World!')
+})
+
+//http://localhost:3000/create/task
+app.post('/create/task', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.post('/create/potato', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
