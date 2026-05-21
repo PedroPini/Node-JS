b27325bb01fcdb3281e66e7e02c70cb7e1aa8650
PedroPini <augustopini@gmail.com>
2026-05-21 18:52:33 +0800

    Adding the models for Task js

--- backend/routes/tasks.js
+++ backend/routes/tasks.js
@@ -1,8 +1,8 @@
 import express from "express";
 const router = express.Router();
+import Task from "../models/Task.js"
 
-
-//GET REQUEST http://localhost:3000/tasks
+//GET REQUEST http://localhost:3000/tasks?name=Max
 router.get("/tasks", async (req, res) => {
     //req -> is the request so the 
     // payload you're receiving in case the user is sending you data
@@ -33,16 +33,16 @@ router.get("/potatoes", async (req, res) => {
 router.post("/create/potatoes", async (req, res) => {
     try {
         console.log(` This is the payload we received ${JSON.stringify(req.body)}`) //[object Object] into string
-        const payload = req.body.class;
-        if(payload == 1){
-            res.status(201).json({"status": "Great class"})
-        } else {
-            res.status(201).json({"status": "Not so Great class"})
-        }
+        const taskName = req.body.taskName; /// const taskName = "Buy Milk"
+        const date = req.body.date;
+        const tags = req.body.tags;
+        res.status(201).json({"status": "Task Created", "task name": taskName, "Task Date": date, "tags": tags})
         
     } catch (error) {
         console.log(`router.get /tasks is failing: ${error}`)
         res.status(404).json({"error": error.code, "message": error.message})
     }
 })
+
+//TODO: POST /create/notes -> note: string, date: string, tag:array
 export default router;
